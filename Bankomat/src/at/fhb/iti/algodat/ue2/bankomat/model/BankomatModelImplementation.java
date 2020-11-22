7426ba5a4b98cb3d98cb3ccd18141f934419562c
Djordja Markovic <markovic.djordja@gmail.com>
2020-11-22 17:08:48 +0100

    ue2 bankomat

--- Bankomat/src/at/fhb/iti/algodat/ue2/bankomat/model/BankomatModelImplementation.java
+++ Bankomat/src/at/fhb/iti/algodat/ue2/bankomat/model/BankomatModelImplementation.java
@@ -8,19 +8,25 @@ public class BankomatModelImplementation implements BankomatModel {
 
 	private BankomatView theView;
 	private BankomatState theState;
+	private int theCode;
 
 	public void setView(BankomatView view) {
 		theView = view;
-		theState = AVAILABLE;
+		theState = START;
+		theCode = 0;
 	}
 	
 	public void pressButtonDigit(int digit) {
 		// TODO Auto-generated method stub
 		theView.setText("Digit " + digit + " pressed.");
 		switch (theState) {
-		case AVAILABLE:
+		case DIGIT0:
 			// TODO Do something according to state
-			theState=STATE1;
+			BankomatState target = DIGIT1;
+			theState=DIGIT1;
+			theCode=digit;
+			theView.setText("Bitte geben Sie Ihren Code ein: *");
+			System.out.println("Edge 2 from "+theState+" to "+target);
 			break;
 		default:
 			break;
@@ -34,7 +40,17 @@ public class BankomatModelImplementation implements BankomatModel {
 
 	public void pressButtonDelete() {
 		// TODO Auto-generated method stub
-		theView.setText("Delete pressed.");				
+		theView.setText("Delete pressed.");
+		// todo
+		switch (theState) {
+			case DIGIT1: {
+				BankomatState target = DIGIT0;
+				theCode=0;
+				System.out.println("Edge (linie ins skizzes) from "+theState+" to "+target);
+				theState=target;
+				break;
+		}
+		}
 	}
 
 	public void pressButtonEnter() {
@@ -51,10 +67,59 @@ public class BankomatModelImplementation implements BankomatModel {
 
 	public void pressButtonCard() {
 		// TODO Auto-generated method stub
-		theView.setText("Card pressed.");
-		theView.setKartenButtonLabel("Karte Button neu");
-		theView.setKartenText("Karte Text neu");
+		//theView.setText("Card pressed.");
+		//theView.setKartenButtonLabel("Karte Button neu");
+		//theView.setKartenText("Karte Text neu");
+		switch(theState) {
+			case START: {
+				BankomatState target = DIGIT0;
+				// TODO semantic
+
+				System.out.println("Edge 1 from "+theState+" to "+target);
+				theState = target;
+			} break;
+			case DIGIT0: {
+				BankomatState target = DIGIT1;
+				System.out.println("Edge 2 from "+theState+" to "+target);
+				theState = target;
+			} break;
+			case DIGIT1: {
+				BankomatState target = DIGIT2;
+				System.out.println("Edge 3 from "+theState+" to "+target);
+				theState = target;
+			} break;
+			case DIGIT2: {
+				BankomatState target = DIGIT3;
+				System.out.println("Edge 4 from "+theState+" to "+target);
+				theState = target;
+			} break;
+			case DIGIT3: {
+				BankomatState target = ENTER;
+				System.out.println("Edge 5 from "+theState+" to "+target);
+				theState = target;
+			} break;
+			case ENTER: {
+				boolean code=true;
+				if (code) {
+					BankomatState target = WAITING;
+					System.out.println("Edge 1 from "+theState+" to "+target);
+					theState = target;
+				} else if (!code) {
+					BankomatState target = DIGIT0;
+					System.out.println("Edge 1 from "+theState+" to "+target);
+					theState = target;
+				}
+			} break;
+		}
 	}
 
+	private String getEdge(Enum<BankomatState> enm) {
+		/*
+		BankomatState target = enm;
+		System.out.println("Edge 1 from "+theState+" to "+target);
+		theState = target;
+		*/
+		return "";
+	}
 
 }
--- Bankomat/src/at/fhb/iti/algodat/ue2/bankomat/model/BankomatState.java
+++ Bankomat/src/at/fhb/iti/algodat/ue2/bankomat/model/BankomatState.java
@@ -2,8 +2,13 @@ package at.fhb.iti.algodat.ue2.bankomat.model;
 
 public enum BankomatState {
 	// TODO Rename States
-	AVAILABLE,
-	STATE1,
-	STATE2,
-	STATE3;
+	START,
+	DIGIT0,
+	DIGIT1,
+	DIGIT2,
+	DIGIT3,
+	ENTER
+	,CANCEL
+	,DELETE
+	,WAITING
 }
